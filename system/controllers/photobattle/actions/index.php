22b39c6c90ec715da551e25096b6a9fa37c37dae
Evgeniy Kirichenko <kirichenko_ev@mail.com>
2017-12-04 23:04:20 +0300

    action по умолчанию

---
+++ system/controllers/photobattle/actions/index.php
@@ -0,0 +1,14 @@
+<?php
+
+class actionPhotobattleIndex extends cmsAction{
+    
+    public function run(){
+        
+        $template = cmsTemplate::getInstance();
+        $battles = array();
+        $template->render('index', array(
+            'battles' => $battles
+        ));
+    }
+
+}
