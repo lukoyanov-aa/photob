a71b965936873b9ca0d1fd0b23e47a3e300353bf
Evgeniy Kirichenko <kirichenko_ev@mail.com>
2017-12-05 23:27:23 +0300

    обработка формы. (не закончено)

--- system/controllers/photobattle/actions/add.php
+++ system/controllers/photobattle/actions/add.php
@@ -9,11 +9,21 @@ class actionPhotobattleAdd extends cmsAction{
         }
         
         $form = $this->getForm('battle'); //выводим созданную форму form_battle
-
+        
         $errors = false; //переменная для записи ошибок валидации
         $template = cmsTemplate::getInstance();
         
-        $battle = array(); // переменная для сбора данных формы
+        // переменная с информацией 
+        $is_submitted = $this->request->has('submit'); //has - проверяет наличие переменной submit в запросе
+        // переменная для сбора данных формы
+        $battle = $form->parse($this->request/* данные введенные пользователем и переданные методом post*/, 
+                                $is_submitted/*булевая переменная была ли отправленна форма*/
+                              ); 
+                              
+        if ($is_submitted){
+            
+            $errors = $form->validate($this, $battle); //валидируем форму
+        }
         
         $template->render('form_battle', array( //рендерим шаблон и передаем в него ряд параметров
             'do' => 'add', // параметр с информацией о действии add - добавление, edit - редактирование
