2397eca1e33c49f802c7da98b26f3c9dfadf13e3
Evgeniy Kirichenko <kirichenko_ev@mail.com>
2017-12-05 14:43:29 +0300

    Добавлен вывод и обработка формы создания фотобитвы

--- system/controllers/photobattle/actions/add.php
+++ system/controllers/photobattle/actions/add.php
@@ -4,10 +4,22 @@ class actionPhotobattleAdd extends cmsAction{
     
     public function run(){
         
+        if (!cmsUser::isAdmin()/*проверяем если текущий пользователь НЕ Администратор*/){
+            cmsCore::error404(); //выводим ошибку 404
+        }
+        
+        $form = $this->getForm('battle'); //выводим созданную форму form_battle
+
+        $errors = false; //переменная для записи ошибок валидации
         $template = cmsTemplate::getInstance();
         
-        $template->render('form_battle', array(
-            'do' => 'add'
+        $battle = array(); // переменная для сбора данных формы
+        
+        $template->render('form_battle', array( //рендерим шаблон и передаем в него ряд параметров
+            'do' => 'add', // параметр с информацией о действии add - добавление, edit - редактирование
+            'form' => $form, // обект формы form_battle
+            'errors' => $errors,
+            'battle' => $battle
         ));
     }
 
