790f112d5592f130c44ab9f067e6ee72463e7b01
Evgeniy Kirichenko <kirichenko_ev@mail.com>
2017-12-07 11:29:34 +0300

    В шаблон добавлены: - Заголовок - глубиномер - кнопки удаления и редактирования

--- templates/default/controllers/photobattle/battle.tpl.php
+++ templates/default/controllers/photobattle/battle.tpl.php
@@ -1,1 +1,33 @@
-<h1><?php html($battle['title']);/*выводим жлемент массива в формате html*/  ?></h1>
+<?php 
+    
+    //добавляем заголовок странициы
+    $this->setPageTitle($battle['title']);
+    
+    //добавляем глубиномер самого компонента
+    $this->addBreadcrumb(LANG_PHOTOBATTLE_CONTROLLER, href_to('photobattle', ''));
+    //добавляем глубиномер действия
+    $this->addBreadcrumb($battle['title']);
+    
+    if (cmsUser::isAdmin()){
+        
+        //добавляем кнопку
+        $this->addToolButton(array(
+            'class' => 'edit',
+            'title' => LANG_PHOTOBATTLE_EDIT,
+            'href' => href_to('photobattle', 'edit', $battle['id'])
+        ));
+        
+        $this->addToolButton(array(
+            'class' => 'delete',
+            'title' => LANG_PHOTOBATTLE_DELETE,
+            'href' => href_to('photobattle', 'delete', $battle['id'])
+        ));
+    }
+
+?>
+
+<h1>
+    <?php 
+        html($battle['title']);//выводим элемент массива и экранируем его 
+    ?>
+</h1>
