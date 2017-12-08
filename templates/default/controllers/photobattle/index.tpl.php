e06d9b3e19ec3bcb809ca91431f17ef54472f300
Evgeniy Kirichenko <kirichenko_ev@mail.com>
2017-12-08 15:25:21 +0300

    Добавлен вывод списка битв

--- templates/default/controllers/photobattle/index.tpl.php
+++ templates/default/controllers/photobattle/index.tpl.php
@@ -10,6 +10,13 @@
             'href' => href_to('photobattle', 'add')
         ));
     }
+    
+    $statuses_text = array(
+		0 => LANG_PHOTOBATTLE_STATUS_PENDING,
+		1 => LANG_PHOTOBATTLE_STATUS_MODERATION,
+		2 => LANG_PHOTOBATTLE_STATUS_OPENED,
+		3 => LANG_PHOTOBATTLE_STATUS_CLOSED,		
+	);
 ?>
 
 <h1> <?php echo LANG_PHOTOBATTLE_CONTROLLER; ?></h1>
@@ -18,3 +25,22 @@
     <p><?php echo LANG_PHOTOBATTLE_NONE; ?></p>
     <?php return; ?>
 <?php } ?>
+
+<div class="photobattles-list">
+	<?php foreach($battles as $battle) { ?>
+		<div class="item">
+			<div class="logo"><?php echo html_image($battle['logo'], 'small') ?></div>
+			<div class="details">
+				<a href="<?php echo $this->href_to('battle', $battle['id']); ?>" class="title"><?php html($battle['title']); ?></a>
+				<div class="info">
+					<span class="status status-<?php echo $battle['status']; ?>">
+						<?php echo $statuses_text[ $battle['status'] ]; ?>
+					</span>
+					<span class="users_count">
+						<?php echo html_spellcount($battle['users_count'], LANG_PHOTOBATTLE_SPELLCOUNT); ?>
+					</span>
+				</div>
+			</div>
+		</div>
+	<?php } ?>
+</div>
